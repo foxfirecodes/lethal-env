aa3cd85c2d6441aa93159410e58f4264b7a2f535
foxfirecodes <foxfirecodes@gmail.com>
2025-01-28 15:15:08 -0500

    improve download-thunderstore.sh

--- download-thunderstore.sh
+++ download-thunderstore.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ -d dev/BepInEx ]; then
+  cd dev
+elif [ ! -d BepInEx ]; then
+  echo "failed to find BepInEx directory. please run this command from inside your dev env" >&1
+  exit 1
+fi
+
 url="${1?missing url}"
 download_zip="$(mktemp)"
 download_folder="$(mktemp -d)"
@@ -8,19 +15,10 @@ echo "downloading $url"
 curl --progress-bar -L "$url" -o "$download_zip"
 unzip -q "$download_zip" -d "$download_folder"
 
-if [ -d "$download_folder"/plugins ]; then
-  echo "moving plugins/*"
-  mv "$download_folder"/plugins/* ./dev/BepInEx/plugins/
-fi
-if [ -d "$download_folder"/config ]; then
-  echo "moving config/*"
-  mv "$download_folder"/config/* ./dev/BepInEx/plugins/
-fi
-if [ -d "$download_folder"/BepInEx/plugins ]; then
-  echo "moving BepInEx/plugins/*"
-  mv "$download_folder"/BepInEx/plugins/* ./dev/BepInEx/plugins/
-fi
-if [ -d "$download_folder"/BepInEx/config ]; then
-  echo "moving BepInEx/config/*"
-  mv "$download_folder"/BepInEx/config/* ./dev/BepInEx/config/
-fi
+mv "$download_folder"/*.dll ./dev/BepInEx/plugins/ 2>/dev/null || true
+mv "$download_folder"/plugins/* ./dev/BepInEx/plugins/ 2>/dev/null || true
+mv "$download_folder"/BepInEx/plugins/* ./dev/BepInEx/plugins/ 2>/dev/null || true
+
+mv "$download_folder"/*.cfg ./dev/BepInEx/config/ 2>/dev/null || true
+mv "$download_folder"/config/* ./dev/BepInEx/config/ 2>/dev/null || true
+mv "$download_folder"/BepInEx/config/* ./dev/BepInEx/config/ 2>/dev/null || true
